4d78837cce4cf5990f95b3de08bd06ed1a3685d8
mcgrady2030524 <luzhenjie2015@163.com>
2018-01-07 20:50:24 +0800

    初次提交

---
+++ springboot-02-view-static/src/main/java/com/itheima/springboot/Application.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.itheima.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+* @ClassName: Application 
+* @Description: TODO(引导类) 
+* @Author Mcgrady
+* @Date 2018年1月6日 下午7:40:21 
+*  
+*/
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
